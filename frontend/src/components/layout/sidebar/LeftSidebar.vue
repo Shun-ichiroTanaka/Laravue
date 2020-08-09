513d3080f4044d4ff339cf958f2e0bcb013fb8ad
田中 俊一朗 <ealbalbs809@gmail.com>
2020-08-09 17:02:34 +0900

    サイドバースロット化　改善余地あり

--- frontend/src/components/layout/sidebar/LeftSidebar.vue
+++ frontend/src/components/layout/sidebar/LeftSidebar.vue
@@ -1,57 +1,83 @@
 <template>
   <div class="relative bg-sidebar hidden sm:block px-2">
     <nav class="text-base font-semibold pt-3 ">
-      <left-sidebar-item></left-sidebar-item>
+      <left-sidebar-item>
+        <book-icon size="20" class="mr-3"></book-icon>
+        Documentation
+        <template #docume>
+          <router-link
+            to="/about"
+            class="flex items-center hover-left opacity-75 hover:opacity-100 py-3 px-2 rounded-full pl-6 nav-item text-gray"
+          >
+            <code-icon size="20" class="mr-3"></code-icon>
+            What is Laravue?
+          </router-link>
+        </template>
+      </left-sidebar-item>
 
-      <router-link
-        to="/setting"
-        class="flex items-center hover-left opacity-75 hover:opacity-100 py-3 px-2 rounded-full pl-6 nav-item text-gray"
-      >
-        <settings-icon size="20" class="mr-3"></settings-icon>
-        Setting
-      </router-link>
-      <router-link
-        to="/tables"
-        class="flex items-center hover-left opacity-75 hover:opacity-100 py-3 px-2 rounded-full pl-6 nav-item text-gray"
-      >
-        <tablet-icon size="20" class="mr-3"></tablet-icon>
-        Tables
-      </router-link>
-      <router-link
-        to="/form"
-        class="flex items-center hover-left opacity-75 hover:opacity-100 py-3 px-2 rounded-full pl-6 nav-item text-gray"
-      >
-        <grid-icon size="20" class="mr-3"></grid-icon>
-        Form
-      </router-link>
-      <router-link
-        to="/darkmode"
-        class="flex items-center hover-left opacity-75 hover:opacity-100 py-3 px-2 rounded-full pl-6 nav-item text-gray"
-      >
-        <circle-icon size="20" class="mr-3"></circle-icon>
-        Darkmode
-      </router-link>
-      <router-link
-        to="/calendar"
-        class="flex items-center hover-left opacity-75 hover:opacity-100 py-3 px-2 rounded-full pl-6 nav-item text-gray"
-      >
-        <calendar-icon size="20" class="mr-3"></calendar-icon>
-        Calendar
-      </router-link>
-      <router-link
-        to="/info"
-        class="flex items-center hover-left opacity-75 hover:opacity-100 py-3 px-2 rounded-full pl-6 nav-item text-gray"
-      >
-        <info-icon size="20" class="mr-3"></info-icon>
-        F&Q
-      </router-link>
-      <router-link
-        to="/chart"
-        class="flex items-center hover-left opacity-75 hover:opacity-100 py-3 px-2 rounded-full pl-6 nav-item text-gray"
-      >
-        <bar-chart-icon size="20" class="mr-3"></bar-chart-icon>
-        Chart
-      </router-link>
+      <left-sidebar-item>
+        <cloud-icon size="20" class="mr-3"></cloud-icon>
+        Components
+        <template #compo>
+          <router-link
+            to="/profile"
+            class="flex items-center hover-left opacity-75 hover:opacity-100 py-3 px-2 rounded-full pl-6 nav-item text-gray"
+          >
+            <user-icon size="20" class="mr-3"></user-icon>
+            Profile
+          </router-link>
+
+          <router-link
+            to="/setting"
+            class="flex items-center hover-left opacity-75 hover:opacity-100 py-3 px-2 rounded-full pl-6 nav-item text-gray"
+          >
+            <settings-icon size="20" class="mr-3"></settings-icon>
+            Setting
+          </router-link>
+          <router-link
+            to="/tables"
+            class="flex items-center hover-left opacity-75 hover:opacity-100 py-3 px-2 rounded-full pl-6 nav-item text-gray"
+          >
+            <tablet-icon size="20" class="mr-3"></tablet-icon>
+            Tables
+          </router-link>
+          <router-link
+            to="/form"
+            class="flex items-center hover-left opacity-75 hover:opacity-100 py-3 px-2 rounded-full pl-6 nav-item text-gray"
+          >
+            <grid-icon size="20" class="mr-3"></grid-icon>
+            Form
+          </router-link>
+          <router-link
+            to="/darkmode"
+            class="flex items-center hover-left opacity-75 hover:opacity-100 py-3 px-2 rounded-full pl-6 nav-item text-gray"
+          >
+            <circle-icon size="20" class="mr-3"></circle-icon>
+            Darkmode
+          </router-link>
+          <router-link
+            to="/calendar"
+            class="flex items-center hover-left opacity-75 hover:opacity-100 py-3 px-2 rounded-full pl-6 nav-item text-gray"
+          >
+            <calendar-icon size="20" class="mr-3"></calendar-icon>
+            Calendar
+          </router-link>
+          <router-link
+            to="/info"
+            class="flex items-center hover-left opacity-75 hover:opacity-100 py-3 px-2 rounded-full pl-6 nav-item text-gray"
+          >
+            <info-icon size="20" class="mr-3"></info-icon>
+            F&Q
+          </router-link>
+          <router-link
+            to="/chart"
+            class="flex items-center hover-left opacity-75 hover:opacity-100 py-3 px-2 rounded-full pl-6 nav-item text-gray"
+          >
+            <bar-chart-icon size="20" class="mr-3"></bar-chart-icon>
+            Chart
+          </router-link>
+        </template>
+      </left-sidebar-item>
     </nav>
   </div>
 </template>
@@ -60,6 +86,7 @@
 
   // icon
   import {
+    UserIcon,
     TabletIcon,
     CalendarIcon,
     SettingsIcon,
@@ -67,6 +94,9 @@
     InfoIcon,
     BarChartIcon,
     CircleIcon,
+    BookIcon,
+    CodeIcon,
+    CloudIcon,
   } from "vue-feather-icons";
   export default {
     name: "LeftSidebar",
@@ -76,6 +106,7 @@
       };
     },
     components: {
+      BookIcon,
       TabletIcon,
       CalendarIcon,
       SettingsIcon,
@@ -84,6 +115,9 @@
       BarChartIcon,
       CircleIcon,
       LeftSidebarItem,
+      CodeIcon,
+      UserIcon,
+      CloudIcon,
     },
   };
 </script>
--- frontend/src/components/layout/sidebar/LeftSidebarItem.vue
+++ frontend/src/components/layout/sidebar/LeftSidebarItem.vue
@@ -4,76 +4,13 @@
       to="/documentation"
       class="flex items-center hover-left py-3 px-2 rounded-full pl-6 nav-item text-gray"
     >
-      <book-icon size="20" class="mr-3 active-icon"></book-icon>
-      Documentation</router-link
-    >
+      <slot></slot>
+    </router-link>
     <transition>
       <div v-if="show" @close="show = false">
         <div class="leftside-bg z-50 absolute w-64 h-screen overflow-hidden">
-          <router-link
-            to="/about"
-            class="flex items-center hover-left opacity-75 hover:opacity-100 py-3 px-2 rounded-full pl-6 nav-item text-gray"
-          >
-            <code-icon size="20" class="mr-3"></code-icon>
-            What is Laravue?
-          </router-link>
-          <router-link
-            to="/profile"
-            class="flex items-center hover-left opacity-75 hover:opacity-100 py-3 px-2 rounded-full pl-6 nav-item text-gray"
-          >
-            <user-icon size="20" class="mr-3"></user-icon>
-            Profile
-          </router-link>
-
-          <router-link
-            to="/setting"
-            class="flex items-center hover-left opacity-75 hover:opacity-100 py-3 px-2 rounded-full pl-6 nav-item text-gray"
-          >
-            <settings-icon size="20" class="mr-3"></settings-icon>
-            Setting
-          </router-link>
-          <router-link
-            to="/tables"
-            class="flex items-center hover-left opacity-75 hover:opacity-100 py-3 px-2 rounded-full pl-6 nav-item text-gray"
-          >
-            <tablet-icon size="20" class="mr-3"></tablet-icon>
-            Tables
-          </router-link>
-          <router-link
-            to="/form"
-            class="flex items-center hover-left opacity-75 hover:opacity-100 py-3 px-2 rounded-full pl-6 nav-item text-gray"
-          >
-            <grid-icon size="20" class="mr-3"></grid-icon>
-            Form
-          </router-link>
-          <router-link
-            to="/darkmode"
-            class="flex items-center hover-left opacity-75 hover:opacity-100 py-3 px-2 rounded-full pl-6 nav-item text-gray"
-          >
-            <circle-icon size="20" class="mr-3"></circle-icon>
-            Darkmode
-          </router-link>
-          <router-link
-            to="/calendar"
-            class="flex items-center hover-left opacity-75 hover:opacity-100 py-3 px-2 rounded-full pl-6 nav-item text-gray"
-          >
-            <calendar-icon size="20" class="mr-3"></calendar-icon>
-            Calendar
-          </router-link>
-          <router-link
-            to="/info"
-            class="flex items-center hover-left opacity-75 hover:opacity-100 py-3 px-2 rounded-full pl-6 nav-item text-gray"
-          >
-            <info-icon size="20" class="mr-3"></info-icon>
-            F&Q
-          </router-link>
-          <router-link
-            to="/chart"
-            class="flex items-center hover-left opacity-75 hover:opacity-100 py-3 px-2 rounded-full pl-6 nav-item text-gray"
-          >
-            <bar-chart-icon size="20" class="mr-3"></bar-chart-icon>
-            Chart
-          </router-link>
+          <slot name="docume" v-if="this.$slots.docume"></slot>
+          <slot name="compo" v-if="this.$slots.compo"></slot>
         </div>
       </div>
     </transition>
@@ -81,18 +18,17 @@
 </template>
 <script>
   // icon
-  import {
-    BookIcon,
-    TabletIcon,
-    UserIcon,
-    CalendarIcon,
-    SettingsIcon,
-    GridIcon,
-    InfoIcon,
-    BarChartIcon,
-    CircleIcon,
-    CodeIcon,
-  } from "vue-feather-icons";
+  import // BookIcon,
+  // TabletIcon,
+  // UserIcon,
+  // CalendarIcon,
+  // SettingsIcon,
+  // GridIcon,
+  // InfoIcon,
+  // BarChartIcon,
+  // CircleIcon,
+  // CodeIcon,
+  "vue-feather-icons";
   export default {
     name: "LeftSidebarItem",
     data() {
@@ -101,16 +37,16 @@
       };
     },
     components: {
-      BookIcon,
-      TabletIcon,
-      UserIcon,
-      CalendarIcon,
-      SettingsIcon,
-      GridIcon,
-      InfoIcon,
-      BarChartIcon,
-      CircleIcon,
-      CodeIcon,
+      // BookIcon,
+      // TabletIcon,
+      // UserIcon,
+      // CalendarIcon,
+      // SettingsIcon,
+      // GridIcon,
+      // InfoIcon,
+      // BarChartIcon,
+      // CircleIcon,
+      // CodeIcon,
     },
     methods: {
       mouseover() {
